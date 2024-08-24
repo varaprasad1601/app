997d92ea689dfd975bf50bed68139248f8ef2fe3
varaprasad1601 <saiprasadvsp1601@gmail.com>
2024-08-24 23:15:52 +0530

    "post removed"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {Provider} from './Components/Context'
-import Post from './Components/Component2';
 import Component2 from './Components/Component2';
 
 function App() {
