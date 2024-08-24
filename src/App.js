288408fe3d83fab6686902ce672654e787c26e97
varaprasad1601 <saiprasadvsp1601@gmail.com>
2024-08-24 23:27:39 +0530

    "ErrorBoundary added"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import {Provider} from './Components/Context'
+import ErrorBoundary from './Components/ErrorBoundary';
 import Component2 from './Components/Component2';
 
 function App() {
   return (
     <>
-      <Provider value='varaprasad'>
+      <ErrorBoundary>
         <Component2 />
-      </Provider>
+      </ErrorBoundary>
     </>
   );
 }
